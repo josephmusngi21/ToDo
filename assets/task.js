a5239624604a2f22e66b4bd3066b56db09eea7cf
Josephmusngi21 <josephmusngi7@gmail.com>
2024-10-08 02:43:16 -0700

    w, added description

--- assets/task.js
+++ assets/task.js
@@ -1,32 +1,37 @@
 export default class Task {
-    constructor(task='', date, type='Progress', tags = ['']) {
+  constructor(task = '', date = new Date(), type = 'Progress', tags = ['+'], description = '') {
       this.task = task;
       this.date = date;
       this.type = type;
       this.tags = tags;
-    }
-  
-    get showTask() {
-      return this.task;
-    }
-  
-    get showDate() {
-      return this.date;
-    }
-  
-    get showType() {
-      return this.type;
-    }
-  
-    showTags(index) {
-      return this.tags[index];
-    }
-  
-    get showAllTags() {
-      return this.tags;
-    }
+      this.description = description; // Ensure description is assigned properly
+  }
 
-    toString() {
-        return `\ntask: ${this.task}\n date: ${this.date}\n type:${this.type}\n tags:${this.tags}`;
-    }
+  showTask() {
+      return this.task;
+  }
+
+  showDate() {
+      return this.date.toString();
+  }
+
+  showType() {
+      return this.type;
+  }
+
+  showTags(index) {
+      return this.tags[index];
+  }
+
+  showAllTags() {
+      return this.tags;
+  }
+
+  showDescription() {
+      return this.description;
+  }
+
+  toString() {
+      return `task: ${this.task}, date: ${this.date}, type: ${this.type}, tags: ${this.tags}, description: ${this.description}`;
+  }
 }
